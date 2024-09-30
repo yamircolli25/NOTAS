7deb24b7a2517f345a73e4cf00a30ada4596c80f
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-09-30 17:17:03 -0600

    .

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalState";
+
+import Header from "./componentes/Header";
+import ColeccionFormulario from "./componentes/ColeccionFormulario";
+import ColeccionListar from "./componentes/ColeccionListar";
+import ColeccionVer from "./componentes/ColeccionVer";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <div className="">
+        <div className="">
+          <Header />
+          <Routes>
+            <Route path="/" element={<ColeccionListar />} />
+            <Route path="/agregar" element={<ColeccionFormulario />} />
+            <Route path="/editar/:id" element={<ColeccionFormulario />} />
+            <Route path="/ver/:id" element={<ColeccionVer />} />
+
+          </Routes>
+        </div>
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
